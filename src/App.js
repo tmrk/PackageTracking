8d0b9119e54a4a170cad92f37e4ba5c884401b47
tmrk <tamas.marki@gmail.com>
2022-05-19 15:43:58 +0200

    Added map, styled basic CSS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Moment from 'react-moment';
 import mockData from './mockData.json';
 
 const jsonAddress = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
 
+const Map = ({lat, long, width = 520, height = 200}) => {
+  const mapboxApiKey = "pk.eyJ1IjoidG1hcmtpIiwiYSI6IlVqTDJabncifQ.b2sMuSa0vQhZe3LekhHGsw";
+  const coords = [long, lat].join(",");
+  const size = [width, height].join("x");
+  const makiIcon = "gift";
+  const iconColour = "D40511";
+  const marker = "pin-s-" + makiIcon + "+" + iconColour + "(" + coords + ")";
+  const mapBoxSrc = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/"
+                  + marker + "/" + coords + ",14/" + size
+                  + "?access_token=" + mapboxApiKey;
+  return (<img src={mapBoxSrc} alt="Map" />);
+}
 
 const Package = ({item}) => {
   return (
     <li>
-      <span>Package #{item.id}</span>
+      <div>
+        <span className="title">Package #{item.id}</span>
+        <span className={"status " + item.status}>{item.status}</span>
+      </div>
       <ul className="info">
-        <li><span>ETA:</span> <span>{item.eta}</span></li>
-        <li><span>Last updated:</span> <span>{item.last_updated}</span></li>
-        <li><span>Latitude:</span> <span>{item.location_coordinate_latitude}</span></li>
-        <li><span>Longitude:</span> <span>{item.location_coordinate_longitude}</span></li>
+        <li><span>ETA:</span> <Moment date={item.eta} durationFromNow /></li>
+        <li><span>Last updated:</span> <Moment date={item.last_updated} format="YYYY-MM-DD hh:mm" /></li>
         <li><span>Location:</span> <span>{item.location_name}</span></li>
-        <li><span>Location status OK:</span> <span>{item.location_status_ok}</span></li>
-        <li><span>Notes:</span> <span>{item.notes}</span></li>
+        <li><span>Location status</span> <span>{item.location_status_ok ? "OK" : "Not OK"}</span></li>
         <li><span>Parcel ID:</span> <span>{item.parcel_id}</span></li>
         <li><span>Sender:</span> <span>{item.sender}</span></li>
-        <li><span>Status:</span> <span>{item.status}</span></li>
         <li><span>User Name:</span> <span>{item.user_name}</span></li>
         <li><span>User Phone:</span> <span>{item.user_phone}</span></li>
-        <li><span>Verification Required:</span> <span>{item.verification_required}</span></li>
+        <li><span>Verification Required:</span> <span>{item.verification_required ? "Yes" : "No"}</span></li>
+        <li><span>Notes:</span> <span>{item.notes}</span></li>
+        <li>
+          <Map lat={item.location_coordinate_latitude} long={item.location_coordinate_longitude} />
+        </li>
       </ul>
     </li>
   );
@@ -44,13 +60,9 @@ const Packages = () => {
     fetchData();
   }, []);
 
-  const handleClick = () => {
-    setPackages([{id: 47389439872}]);
-  }
-
   return (
     <div id="packages">
-      <ul onClick={handleClick}>
+      <ul>
         {packages.map((item) => (
           <Package key={item.id} item={item} />
         ))}
